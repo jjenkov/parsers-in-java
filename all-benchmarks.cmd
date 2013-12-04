6a0b3a578e7e78fc94093feb952ea91b9bd87ea6
Jakob Jenkov <jakob@jenkov.com>
2013-12-04 20:30:16 +0100

    Benchmark .cmd files added
    Scripts for running the benchmarks on Windows.

---
+++ all-benchmarks.cmd
@@ -0,0 +1,102 @@
+call run-benchmark.cmd GsonStream data/small.json.txt 10000000
+call run-benchmark.cmd GsonStream data/small.json.txt 10000000
+call run-benchmark.cmd GsonStream data/small.json.txt 10000000
+
+call run-benchmark.cmd GsonStream data/medium.json.txt 1000000
+call run-benchmark.cmd GsonStream data/medium.json.txt 1000000
+call run-benchmark.cmd GsonStream data/medium.json.txt 1000000
+
+call run-benchmark.cmd GsonStream data/large.json.txt 1000000
+call run-benchmark.cmd GsonStream data/large.json.txt 1000000
+call run-benchmark.cmd GsonStream data/large.json.txt 1000000
+
+
+call run-benchmark.cmd GsonStreamBuilder data/small.json.txt 10000000
+call run-benchmark.cmd GsonStreamBuilder data/small.json.txt 10000000
+call run-benchmark.cmd GsonStreamBuilder data/small.json.txt 10000000
+
+call run-benchmark.cmd GsonStreamBuilder data/medium.json.txt 1000000
+call run-benchmark.cmd GsonStreamBuilder data/medium.json.txt 1000000
+call run-benchmark.cmd GsonStreamBuilder data/medium.json.txt 1000000
+
+call run-benchmark.cmd GsonStreamBuilder data/large.json.txt 1000000
+call run-benchmark.cmd GsonStreamBuilder data/large.json.txt 1000000
+call run-benchmark.cmd GsonStreamBuilder data/large.json.txt 1000000
+
+
+call run-benchmark.cmd GsonMap data/small.json.txt 10000000
+call run-benchmark.cmd GsonMap data/small.json.txt 10000000
+call run-benchmark.cmd GsonMap data/small.json.txt 10000000
+
+call run-benchmark.cmd GsonMap data/medium.json.txt 1000000
+call run-benchmark.cmd GsonMap data/medium.json.txt 1000000
+call run-benchmark.cmd GsonMap data/medium.json.txt 1000000
+
+call run-benchmark.cmd GsonMap data/large.json.txt 1000000
+call run-benchmark.cmd GsonMap data/large.json.txt 1000000
+call run-benchmark.cmd GsonMap data/large.json.txt 1000000
+
+
+call run-benchmark.cmd GsonReflection data/small.json.txt 10000000
+call run-benchmark.cmd GsonReflection data/small.json.txt 10000000
+call run-benchmark.cmd GsonReflection data/small.json.txt 10000000
+
+call run-benchmark.cmd GsonReflection data/medium.json.txt 1000000
+call run-benchmark.cmd GsonReflection data/medium.json.txt 1000000
+call run-benchmark.cmd GsonReflection data/medium.json.txt 1000000
+
+call run-benchmark.cmd GsonReflection data/large.json.txt 1000000
+call run-benchmark.cmd GsonReflection data/large.json.txt 1000000
+call run-benchmark.cmd GsonReflection data/large.json.txt 1000000
+
+
+call run-benchmark.cmd JsonParser2 data/small.json.txt 10000000
+call run-benchmark.cmd JsonParser2 data/small.json.txt 10000000
+call run-benchmark.cmd JsonParser2 data/small.json.txt 10000000
+
+call run-benchmark.cmd JsonParser2 data/medium.json.txt 1000000
+call run-benchmark.cmd JsonParser2 data/medium.json.txt 1000000
+call run-benchmark.cmd JsonParser2 data/medium.json.txt 1000000
+
+call run-benchmark.cmd JsonParser2 data/large.json.txt 1000000
+call run-benchmark.cmd JsonParser2 data/large.json.txt 1000000
+call run-benchmark.cmd JsonParser2 data/large.json.txt 1000000
+
+
+call run-benchmark.cmd JsonParser data/small.json.txt 10000000
+call run-benchmark.cmd JsonParser data/small.json.txt 10000000
+call run-benchmark.cmd JsonParser data/small.json.txt 10000000
+
+call run-benchmark.cmd JsonParser data/medium.json.txt 1000000
+call run-benchmark.cmd JsonParser data/medium.json.txt 1000000
+call run-benchmark.cmd JsonParser data/medium.json.txt 1000000
+
+call run-benchmark.cmd JsonParser data/large.json.txt 1000000
+call run-benchmark.cmd JsonParser data/large.json.txt 1000000
+call run-benchmark.cmd JsonParser data/large.json.txt 1000000
+
+
+call run-benchmark.cmd JsonParser2Builder data/small.json.txt 10000000
+call run-benchmark.cmd JsonParser2Builder data/small.json.txt 10000000
+call run-benchmark.cmd JsonParser2Builder data/small.json.txt 10000000
+
+call run-benchmark.cmd JsonParser2Builder data/medium.json.txt 1000000
+call run-benchmark.cmd JsonParser2Builder data/medium.json.txt 1000000
+call run-benchmark.cmd JsonParser2Builder data/medium.json.txt 1000000
+
+call run-benchmark.cmd JsonParser2Builder data/large.json.txt 1000000
+call run-benchmark.cmd JsonParser2Builder data/large.json.txt 1000000
+call run-benchmark.cmd JsonParser2Builder data/large.json.txt 1000000
+
+
+call run-benchmark.cmd JsonParserBuilder data/small.json.txt 10000000
+call run-benchmark.cmd JsonParserBuilder data/small.json.txt 10000000
+call run-benchmark.cmd JsonParserBuilder data/small.json.txt 10000000
+
+call run-benchmark.cmd JsonParserBuilder data/medium.json.txt 1000000
+call run-benchmark.cmd JsonParserBuilder data/medium.json.txt 1000000
+call run-benchmark.cmd JsonParserBuilder data/medium.json.txt 1000000
+
+call run-benchmark.cmd JsonParserBuilder data/large.json.txt 1000000
+call run-benchmark.cmd JsonParserBuilder data/large.json.txt 1000000
+call run-benchmark.cmd JsonParserBuilder data/large.json.txt 1000000
---
+++ run-benchmark.cmd
@@ -0,0 +1,1 @@
+java -cp D:\data\java\gson\google-gson-2.2.4\gson-2.2.4.jar;out\production\parsers; com.jenkov.parsers.AllBenchmarks %1 %2 %3
